083018de1ffbda3c918ea4a90749e73c7f5fa1b7
Earl Adams <earladams2000@gmail.com>
2022-06-06 11:36:01 -0400

    setting up css in about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/pro.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
 
 const about = () => {
   return (
@@ -8,8 +13,35 @@ const about = () => {
       <h2>About Me</h2>
 
       <div className="container about__container"> 
-      
-      
+      <div className='about__me'>
+         <div className='about__me-image'>
+           <img src={ME} alt="About Image" />
+         </div>
+
+      </div>
+      <div className='about__content'>
+        <div className='about__cards'>
+          <article className='about__card'>
+             <FaAward className='about__icon'/> 
+            <h5>Experience</h5>
+            <small>2+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+             <FiUsers className='about__icon'/> 
+            <h5>Clients</h5>
+            <small>25+ locally</small>
+            </article>
+
+            <article className='about__card'>
+             <VscFolderLibrary className='about__icon'/> 
+            <h5>Projects</h5>
+            <small>3+ Completed Projects</small>
+            </article>
+        </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam cum voluptate iste eaque nesciunt nulla necessitatibus quaerat ratione harum quidem dolorum, reiciendis, pariatur porro minima, quasi culpa officiis ducimus exercitationem.</p>
+        <a href="#contact" className='btn btn-primary'>Lets Talk</a>
+      </div>
       
       </div>
     </section>
