2e0999a38bb7099e8b87f6a890d34ebc5c979f95
Earl Adams <earladams2000@gmail.com>
2022-06-06 18:15:51 -0400

    email js buggy

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,6 +4,7 @@ import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
 
+
 const Contact = () => {
   return (
     <section id='contact'>
@@ -13,19 +14,19 @@ const Contact = () => {
       <div className='container contact__container'>
         <div className='contact__options'>
         <article className='contact__option'>
-          <MdOutlineEmail/>
+          <MdOutlineEmail className='contact__option-icon'/>
           <h4>Email</h4>
           <h5>My Email</h5>
           <a href="mailto:earladams200@gmail.com" target='_blank'>Send Me A Message</a>
         </article>
         <article className='contact__option'>
-          <RiMessengerLine/>
+          <RiMessengerLine className='contact__option-icon'/>
           <h4>Messenger</h4>
           <h5>E.Adams</h5>
           <a href="" target='_blank'>Send Me A Message </a>
         </article>
         <article className='contact__option'>
-          <BsWhatsapp/>
+          <BsWhatsapp className='contact__option-icon'/>
           <h4>Whatsapp</h4>
           <h5>+123456789</h5>
           <a href=""target='_blank'>Send Me A Message</a>
