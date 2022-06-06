ab8d6dc716906753221731362e54930cff7679f2
Earl Adams <earladams2000@gmail.com>
2022-06-06 11:07:17 -0400

    fixed problem with nav

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,16 @@ import './about.css'
 
 const about = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container"> 
+      
+      
+      
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -10,13 +10,13 @@ import {useState} from 'react'
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>
+    <nav>
       <a href="#" className= {activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
       <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
-    </div>
+    </nav>
   )
 }
 
